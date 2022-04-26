4a7d94b81c375287c61516faa1ca3b249f72c3f6
GriffenVoj <96258610+GriffenVoj@users.noreply.github.com>
2022-04-26 10:35:51 -0700

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,50 @@
+import random
+words=['abruptly', 'absurd', 'abyss', 'affix', 'askew', 'avenue', 'awkward', 'axiom', 'azure', 'bagpipes', 'bandwagon', 'banjo', 'bayou', 'beekeeper', 'bikini', 'blitz', 'blizzard', 'boggle', 'bookworm', 'boxcar', 'boxful', 'buckaroo', 'buffalo', 'buffoon', 'buxom', 'buzzard', 'buzzing', 'buzzwords', 'caliph', 'cobweb', 'cockiness', 'croquet', 'crypt', 'curacao', 'cycle', 'daiquiri', 'dirndl', 'disavow', 'dizzying', 'duplex', 'dwarves', 'embezzle', 'equip', 'espionage', 'euouae', 'exodus', 'faking', 'fishhook', 'fixable', 'fjord', 'flapjack', 'flopping', 'fluffiness', 'flyby', 'foxglove', 'frazzled', 'frizzled', 'fuchsia', 'funny', 'gabby', 'galaxy', 'galvanize', 'gazebo', 'giaour', 'gizmo', 'glowworm', 'glyph', 'gnarly', 'gnostic', 'gossip', 'grogginess', 'haiku', 'haphazard', 'hyphen', 'iatrogenic', 'icebox', 'injury', 'ivory', 'ivy', 'jackpot', 'jaundice', 'jawbreaker', 'jaywalk', 'jazziest', 'jazzy', 'jelly', 'jigsaw', 'jinx', 'jiujitsu', 'jockey', 'jogging', 'joking', 'jovial', 'joyful', 'juicy', 'jukebox', 'jumbo', 'kayak', 'kazoo', 'keyhole', 'khaki', 'kilobyte', 'kiosk', 'kitsch', 'kiwifruit', 'klutz', 'knapsack', 'larynx', 'lengths', 'lucky', 'luxury', 'lymph', 'marquis', 'matrix', 'megahertz', 'microwave', 'mnemonic', 'mystify', 'naphtha', 'nightclub', 'nowadays', 'numbskull', 'nymph', 'onyx', 'ovary', 'oxidize', 'oxygen', 'pajama', 'peekaboo', 'phlegm', 'pixel', 'pizazz', 'pneumonia', 'polka', 'pshaw', 'psyche', 'puppy', 'puzzling', 'quartz', 'queue', 'quips', 'quixotic', 'quiz', 'quizzes', 'quorum', 'razzmatazz', 'rhubarb', 'rhythm', 'rickshaw', 'schnapps', 'scratch', 'shiv', 'snazzy', 'sphinx', 'spritz', 'squawk', 'staff', 'strength', 'strengths', 'stretch', 'stronghold', 'stymied', 'subway', 'swivel', 'syndrome', 'thriftless', 'thumbscrew', 'topaz', 'transcript', 'transgress', 'transplant', 'triphthong', 'twelfth', 'twelfths', 'unknown', 'unworthy', 'unzip', 'uptown', 'vaporize', 'vixen', 'vodka', 'voodoo', 'vortex', 'voyeurism', 'walkway', 'waltz', 'wave', 'wavy', 'waxy', 'wellspring', 'wheezy', 'whiskey', 'whizzing', 'whomever', 'wimpy', 'witchcraft', 'wizard', 'woozy', 'wristwatch', 'wyvern', 'xylophone', 'yachtsman', 'yippee', 'yoked', 'youthful', 'yummy', 'zephyr', 'zigzag', 'zigzagging', 'zilch', 'zipper', 'zodiac', 'zombie']
+secret_word=random.choice(words)
+second_word=[]
+for letter in secret_word:
+    second_word.append(letter)
+guesses=[]
+word=["_"]*len(secret_word)
+ready=False
+print(len(secret_word))
+failure_count=int(input("The word is that many letters long, how many tries do you want?"))
+ready=True
+print("Type give up to end your attempt and see the word!")
+while failure_count>0 and ready==True:
+    guess=input("Enter a letter:")
+    print(word)
+    if guess == "_":
+        print("Please Try Again")
+    elif guess=="give up":
+        failure_count=0
+    elif guess in guesses:
+        print("You've already tried that letter")
+    elif guess in secret_word and guess.isalpha():
+        
+        print(f"Correct! There is one or more {guess} in secret word")
+        sub=secret_word.count(guess)
+        for i in range(sub):
+            secondsub=secret_word.find(guess)
+            word[secondsub]=guess
+            secret_word=secret_word.replace(guess," ",1)
+        print(word)
+        print(guesses)
+    elif guess not in secret_word:
+        guesses.append(guess)
+        print("Sorry that is not in the word")
+        failure_count-=1
+        print("tries left:",failure_count)
+        print("guessed letters",guesses)
+    if word==second_word:
+        substring=""
+        for letter in second_word:
+            substring+=letter
+        print("you win! the word was:",substring)
+        break
+    
+if failure_count==0:
+    substring=""
+    for letter in second_word:
+        substring+=letter
+    print("Sorry, the word was:", substring, "try again!")
